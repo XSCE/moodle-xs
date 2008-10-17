e4114900c1dbc4330ba2856da6dce8b03cda48c7
root <root@schoolserver1.(none)>
2008-10-17 05:21:54 -0400

    auth/olpcxs: first user to login becomes sitewide 'coursecreator'
    And the 'cc' role gets some extra abilities.

--- auth/olpcxs/auth.php
+++ auth/olpcxs/auth.php
@@ -50,6 +50,15 @@ class auth_plugin_olpcxs extends auth_plugin_base {
 	return $localuser;
       }
 
+      $first = get_field_sql("SELECT COUNT(id)
+                              FROM {$CFG->prefix}user
+                              WHERE auth='olpcxs'");
+      if ((int)$first > 0) {
+	$first = true;
+      } else {
+	$first = false;
+      }
+
       // add the user to the database if necessary
       $user = new StdClass;
       $user->username = addslashes($extuser['serial']);
@@ -65,6 +74,25 @@ class auth_plugin_olpcxs extends auth_plugin_base {
 
       if (! $localuser = get_record('user', 'id', $uid)) {
 	error("Failed to get new user record");
+      }
+
+      if ($first) {
+	$ccrole  = get_record('role', 'shortname', 'coursecreator');
+	$etrole  = get_record('role', 'shortname', 'editingteacher');
+	$sitectx = get_context_instance(CONTEXT_SYSTEM);
+	$sitecoursectx = get_record('context',
+				    'contextlevel', CONTEXT_COURSE,
+				    'instanceid', SITEID);
+
+	role_assign($ccrole->id, $localuser->id, 0, $sitectx->id);
+	role_assign($ccrole->id, $localuser->id, 0, $sitecoursectx->id);
+	role_assign($etrole->id, $localuser->id, 0, $sitecoursectx->id);
+
+	// tweak coursecreator to be able to assign course-creator roles systemwide...
+	assign_capability('moodle/role:assign', CAP_ALLOW, $ccrole->id, $sitectx->id, false);
+	if (!get_record('role_allow_assign', 'roleid', $ccrole->id, 'allowassign', $ccrole->id)) {
+	  allow_assign($ccrole->id,$ccrole->id);
+	}
       }
 
       return $localuser;
