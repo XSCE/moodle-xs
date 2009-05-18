9e1a36344f9abadbb618a5e9ddd4b9dbcfb77eac
Martin Langhoff <martin@laptop.org>
2009-05-18 12:02:24 -0400

    xsconfig: admin settings now require moodle/local:xsconfig

--- local/settings.php
+++ local/settings.php
@@ -4,14 +4,15 @@
  *
  */
 
-if (has_capability('moodle/local:editaliases', $systemcontext)) {
-
+if (has_capability('moodle/local:xsconfig', $systemcontext)) {
 // "presence" settingpage
-    $temp = new admin_settingpage('presence', get_string('presence', 'olpcxs'));
+    $temp = new admin_settingpage('presence', get_string('presence', 'olpcxs'),
+                                  'moodle/local:xsconfig');
     $temp->add(new admin_setting_configcheckbox('presencebycourse',
                                                 get_string('presencebycourse', 'olpcxs'),
                                                 get_string('configpresencebycourse', 'olpcxs'),
                                                 0));
     $ADMIN->add('courses', $temp);
 }
+
 ?>
--- local/version.php
+++ local/version.php
@@ -1,5 +1,5 @@
 <?php 
 
-$local_version = '2009042801';
+$local_version = '2009051800';
 
 ?>
