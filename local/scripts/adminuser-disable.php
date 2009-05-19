7f0c4b52f4c31c1c5c32cccb4df353603aaaad28
Martin Langhoff <martin@laptop.org>
2009-05-19 14:18:58 -0400

    Introduce adminuser-{enable,disable} and use them to disable admin after install
    We re-enable the admin acct during upgrades, as we need an admin account
    to masquerade as, and because various routines during upgrade will try
    to create an admin acct if there isn't one.

---
+++ local/scripts/adminuser-disable.php
@@ -0,0 +1,20 @@
+<?php 
+//Intruders OUT!!!
+if (!empty($_SERVER['GATEWAY_INTERFACE'])){
+    error_log(__FILE__ . ' should not be called from apache!');
+    echo 'This script is not accessible from the webserver';
+    exit;
+}
+define('FULLME', 'cron');
+$nomoodlecookie = true;
+require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
+
+if (set_field('user',
+              'deleted', 1,
+              'username', 'admin',
+              'auth', 'manual')) {
+    exit(0);
+} else { 
+    exit(1);
+}
+?>
